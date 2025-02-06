01f040bc458f0b0a9a0088e84ee766973e0af0e0
jazhao28 <jazhao28@gmail.com>
2025-02-05 19:47:22 -0800

    Created flashcard page and link to it on Study Techniques

---
+++ app/flashcard/page.tsx
@@ -0,0 +1,4 @@
+export default function FlashCard(){
+
+    return <h1>FlashCard Stuff</h1>
+}
--- app/study-techniques/page.tsx
+++ app/study-techniques/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function StudyTechniques() {
     return (
       <div className="flex flex-col items-center justify-center p-8">
         <h1 className="text-2xl font-bold mb-4">Study Techniques</h1>
         <p>Explore various study techniques to boost your productivity.</p>
+        <h2><Link href="/flashcard">FlashCard Stuff</Link></h2>
       </div>
+      
     )
   }
   
