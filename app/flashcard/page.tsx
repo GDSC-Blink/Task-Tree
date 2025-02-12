5568965b7738982630c7288fb5167e45260d975c
jazhao28 <jazhao28@gmail.com>
2025-02-12 15:54:22 -0800

    added searchbar, creating decks

--- app/flashcard/page.tsx
+++ app/flashcard/page.tsx
@@ -1,4 +1,12 @@
+"use client"
+
+import FlashCardForm from "@/components/FlashcardForm"
 export default function FlashCard(){
 
-    return <h1>FlashCard Stuff</h1>
+    return <>
+        <h1 className="text-center font-bold text-3xl pt-8">Create FlashCard Sets</h1>
+            <FlashCardForm/>
+
+        
+    </>
 }
